a40495e45608e025b8da748c84a8280ebcf722cd
Daniel Cataño <dasniel19910@gmail.com>
2018-04-06 21:41:27 -0500

    --Registration css sheet imported in login stylesheet --Login validation (Not empty fields) --Logout button floated to the left of the navigation bar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SessionService } from '../../services/session.service';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog'
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +10,31 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dial
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  height: number;
+  loginForm;
 
-  constructor(
-    private router: Router,
-    private sessionService: SessionService
-  ) { }
+  constructor(private router: Router, private sessionService: SessionService) { 
+    this.loginForm = new FormGroup({
+      username: new FormControl("", Validators.compose([
+        Validators.required,
+      ])),      
+      password: new FormControl("", Validators.compose([
+        Validators.required,
+      ])),
+    })
+  }
 
   ngOnInit() {
+    this.height = window.innerHeight;
   }
 
   login(form: NgForm) {
-
     if (this.sessionService.verifyUser(form.value.username, form.value.password)) {
       this.router.navigate(['/topicsToBeOpen']);
-    }
-    
+    }   
   }
 
   register(){
     this.router.navigate(['/register']);
   }
-
 }
